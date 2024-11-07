a0a27c4784bba1eaa975d1d74442c51f2b07d034
abhiayasya <168184952+abhiayasya@users.noreply.github.com>
2024-11-07 18:31:25 +0530

    stepper component

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,4 +1,5 @@
 import AchieveALotCard from "@/components/AchieveALotCard/AchieveALotCard";
+import Stepper from "@/components/Stepper/Stepper";
 
 export default function Home() {
   const cardData = [
@@ -41,26 +42,41 @@ export default function Home() {
   ];
 
   return (
-    <div className="bg-gray-200 p-8 flex-col flex gap-[66px] px-[141px]">
-      <div className="flex flex-col items-center">
-        <h1 className="font-extrabold text-[80px]">
-          Achieve A Lot <span className="text-red-500">With...</span>
-        </h1>
-        <h3 className="text-[20px]">
-          Sollicitudin vitae eu nulla amet ut vestibulum senectus enim. Etiam
-          cras sapien ipsum mi sit urna auctor facilisi. Mollis
-        </h3>
+    <div>
+      <div className="bg-gray-200 p-8 flex-col flex gap-[66px] px-[141px]">
+        <div className="flex flex-col items-center">
+          <h1 className="font-extrabold text-[50px]">
+            Achieve A Lot <span className="text-red-500">With...</span>
+          </h1>
+          <h3 className="text-[20px]">
+            Sollicitudin vitae eu nulla amet ut vestibulum senectus enim. Etiam
+            cras sapien ipsum mi sit urna auctor facilisi. Mollis
+          </h3>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center items-center bg-gray-200 gap-8">
+          {cardData.map((card, index) => (
+            <AchieveALotCard
+              key={index}
+              icon={card.icon}
+              title={card.title}
+              description={card.description}
+              className={card.className}
+            />
+          ))}
+        </div>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center items-center bg-gray-200 gap-8">
-        {cardData.map((card, index) => (
-          <AchieveALotCard
-            key={index}
-            icon={card.icon}
-            title={card.title}
-            description={card.description}
-            className={card.className}
-          />
-        ))}
+      <div className="bg-gray-200 flex-col flex  px-[141px] gap-[30px] py-10">
+        <div className="flex flex-col items-center">
+          <h1 className="font-extrabold text-[50px] tracking-[-3.5px] ">
+          How To Pay On Call With  <span className="text-red-500">UPI123</span>
+          </h1>
+          <h3 className="text-[20px] tracking-[0px]">
+          Sollicitudin vitae eu nulla amet ut vestibulum senectus enim. Etiam cras sapien ipsum mi sit urna auctor facilisi. Mollis
+          </h3>
+        </div>
+        <div>
+          <Stepper />
+        </div>
       </div>
     </div>
   );
