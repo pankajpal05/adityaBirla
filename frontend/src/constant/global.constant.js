c0a60e76df268c9cf9e48bd1fe915746c3f627ba
pankajpal05 <pankaj.pal@ayasya.com>
2024-11-08 13:32:23 +0530

    update tailwind config

--- frontend/src/constant/global.constant.js
+++ frontend/src/constant/global.constant.js
@@ -1,15 +1,15 @@
 const TEMPLATE_VIEW_BORDER_COLOR = [
-  "!border-b-red-500",
+  "border-b-red-500",
 
-  "!border-b-green-500",
+  "border-b-green-500",
 
-  "!border-b-blue-500",
+  "border-b-blue-500",
 
-  "!border-b-green-500",
+  "border-b-green-500",
 
-  "!border-b-red-500",
+  "border-b-red-500",
 
-  "!border-b-pink-500",
+  "border-b-pink-500",
 ];
 
 const STEPPER_BORDER_COLOR = [
